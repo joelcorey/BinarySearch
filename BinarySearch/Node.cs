891a7df5eff89d7eb0fab69c6d80a69d08ad4317
joel <joeloregonmines@gmail.com>
2018-04-13 10:26:04 -0600

    Re-init

---
+++ BinarySearch/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Node
+    {
+
+        public Node(data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+}
